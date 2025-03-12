f5cbdeb0808d79d5734279842282845b44f3552c
lechartfon <lechartfon1@educacion.navarra.es>
2025-03-12 12:22:44 +0100

    Primer commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Clase para realizar operaciones matemáticas.
+ * @package echarteFonseca.DEAW06
+ */
+class Operaciones {
+    /**
+     * Suma dos números.
+     * @param float $a Primer número.
+     * @param float $b Segundo número.
+     * @return float Resultado de la suma.
+     */
+    public function suma($a, $b) {
+        return $a + $b;
+    }
+
+    /**
+     * Resta dos números.
+     * @param float $a Primer número.
+     * @param float $b Segundo número.
+     * @return float Resultado de la resta.
+     */
+    public function resta($a, $b) {
+        return $a - $b;
+    }
+
+    /**
+     * Multiplica dos números.
+     * @param float $a Primer número.
+     * @param float $b Segundo número.
+     * @return float Resultado de la multiplicación.
+     */
+    public function multiplicacion($a, $b) {
+        return $a * $b;
+    }
+}
+?>
